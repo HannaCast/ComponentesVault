10b8a1dabc6d56c6c821ad4e5ba4c59821d797ca
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-03-18 22:40:56 -0600

    [ADD] Implementación de view en subjects

--- horarios_backend/subjects/views/__init__.py
+++ horarios_backend/subjects/views/__init__.py
@@ -1,1 +1,2 @@
 from .colors import ColorListView, ColorPaginatedView, ColorDetailView, ColorToggleStatusView
+from .subjects import SubjectListView, SubjectPaginatedView, SubjectDetailView, SubjectToggleStatusView
