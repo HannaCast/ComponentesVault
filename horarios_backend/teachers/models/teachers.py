21b0495eca23ffa0f74a45b0b6b425b7166b85e8
Hanna Romybsi Pérez Caspeta <168793032+HannaCast@users.noreply.github.com>
2026-03-28 14:46:51 -0600

    [UPDATE] Continuacion de correcciones en el archivo de teachers.py

--- horarios_backend/teachers/models/teachers.py
+++ horarios_backend/teachers/models/teachers.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Teachers(models.Model):
-    name = models.CharField(max_length=45)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    required_classroom = models.IntegerField()
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    require_classroom = models.IntegerField()
     status = models.IntegerField()
     is_deleted = models.IntegerField()
     created_at = models.DateTimeField(blank=True, null=True)
