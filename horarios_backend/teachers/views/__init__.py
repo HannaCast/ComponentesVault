7e3c6a88ba18d40f09f380efc2ec0267ad0a7f82
Hanna Romybsi Pérez Caspeta <168793032+HannaCast@users.noreply.github.com>
2026-03-30 19:52:55 -0600

    [ADD] Vistas REST para CRUD y toggle de disponibilidades de profesor

--- horarios_backend/teachers/views/__init__.py
+++ horarios_backend/teachers/views/__init__.py
@@ -1,1 +1,7 @@
 from .teachers import TeacherListView, TeacherPaginatedView, TeacherDetailView, TeacherToggleStatusView
+from .teacher_availabilities import (
+    TeacherAvailabilityListView,
+    TeacherAvailabilityPaginatedView,
+    TeacherAvailabilityDetailView,
+    TeacherAvailabilityToggleView,
+)
