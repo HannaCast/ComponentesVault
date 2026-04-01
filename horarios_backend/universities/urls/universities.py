d8e2424d02c5b94fde7ee83677915dc1f54305e1
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-03-31 23:21:45 -0600

    [UPDATE] Corrección de la estructura de URLs en university

--- horarios_backend/universities/urls/universities.py
+++ horarios_backend/universities/urls/universities.py
@@ -2,7 +2,7 @@ from django.urls import path
 from universities.views import UniversityCreate, UniversityList, UniversityDetail
 
 urlpatterns = [
-    path('universities/', UniversityList.as_view()),
-    path('universities/create/', UniversityCreate.as_view()),
-    path('universities/<int:university_id>/', UniversityDetail.as_view()),
+    path('v1/universities/', UniversityList.as_view()),
+    path('v1/universities/create/', UniversityCreate.as_view()),
+    path('v1/universities/<int:university_id>/', UniversityDetail.as_view()),
 ]
