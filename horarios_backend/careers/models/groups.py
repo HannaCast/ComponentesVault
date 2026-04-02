0c1c7ed689133ad6bc0d6c6a71424508d2c9e49b
Oscar Uriel Montes Patiño <20233tn073@utez.edu.mx>
2026-04-01 19:13:02 -0600

    [ADD] agregación del modelo de grupos

---
+++ horarios_backend/careers/models/groups.py
@@ -0,0 +1,23 @@
+from django.db import models
+from horarios_backend.universities.models.academic_periods import AcademicPeriods
+from universities.models.universities import Universities
+from careers.models.careers import Careers
+class Groups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    career = models.ForeignKey(Careers, models.DO_NOTHING)
+    period_number = models.IntegerField()
+    letter = models.CharField(max_length=1)
+    shift = models.ForeignKey('Shifts', models.DO_NOTHING)
+    academic_period = models.ForeignKey(AcademicPeriods, models.DO_NOTHING, blank=True, null=True)
+    university = models.ForeignKey('Universities', models.DO_NOTHING)
+    status = models.IntegerField()
+    is_deleted = models.IntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    created_by = models.CharField(max_length=100, blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    updated_by = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'groups'
