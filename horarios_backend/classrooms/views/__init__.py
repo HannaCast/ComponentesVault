dc15a86e144a9163068f0cf063c90dd6e259af16
AlexAviles345 <20233tn198@utez.edu.mx>
2026-04-03 21:09:03 -0600

    [ADD] Entidad de classroom_careers

--- horarios_backend/classrooms/views/__init__.py
+++ horarios_backend/classrooms/views/__init__.py
@@ -1,4 +1,8 @@
 from .classroom_types import ClassroomTypesListView
+from .classroom_careers import (
+    ClassroomCareerDetailView,
+    ClassroomCareerListView,
+)
 from .classrooms import (
     ClassroomListView,
     ClassroomPaginatedView,
