8498d8808209019108841d299d685ba5f7f37efb
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-04-03 20:30:15 -0600

    [UPDATE] Ajustes en la configuración de URLs

--- horarios_backend/universities/urls/__init__.py
+++ horarios_backend/universities/urls/__init__.py
@@ -1,11 +1,8 @@
 from django.urls import include, path
 
 urlpatterns = [
-    # Endpoints existentes de universidades
     path('', include('universities.urls.universities')),
-
-    # Nuevos módulos
     path('', include('universities.urls.period_types')),
-
+    path('', include('universities.urls.shifts')),
     path('', include('universities.urls.academic_periods')),
 ]
