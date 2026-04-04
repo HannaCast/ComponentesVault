f0ab22ef46a1db67781bfc0f33e779e25174e9d9
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-04-03 22:56:50 -0600

    [ADD]Implementacion de urls para teachers universities

--- horarios_backend/teachers/urls/teachers.py
+++ horarios_backend/teachers/urls/teachers.py
@@ -1,9 +1,27 @@
 from django.urls import path
-from teachers.views import TeacherListView, TeacherPaginatedView, TeacherDetailView, TeacherToggleStatusView
+
+from teachers.views import (
+    TeacherDetailView,
+    TeacherListView,
+    TeacherPaginatedView,
+    TeacherToggleStatusView,
+)
 
 urlpatterns = [
-    path('v1/teachers/', TeacherListView.as_view()),
-    path('v1/teachers/paginated/', TeacherPaginatedView.as_view()),
-    path('v1/teachers/<int:pk>/', TeacherDetailView.as_view()),
-    path('v1/teachers/<int:pk>/toggle-status/', TeacherToggleStatusView.as_view()),
+    path('v1/university/teachers/', TeacherListView.as_view(), name='teacher-list'),
+    path(
+        'v1/university/teachers/paginated/',
+        TeacherPaginatedView.as_view(),
+        name='teacher-paginated',
+    ),
+    path(
+        'v1/university/teachers/<int:pk>/',
+        TeacherDetailView.as_view(),
+        name='teacher-detail',
+    ),
+    path(
+        'v1/university/teachers/<int:pk>/toggle-status/',
+        TeacherToggleStatusView.as_view(),
+        name='teacher-toggle-status',
+    ),
 ]
