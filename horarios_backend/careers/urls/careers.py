26f9488fba6b99b7c39ae8e4f9bc81f3ae12a8e6
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-04-03 20:00:43 -0600

    [UPDATE] Ajustes en la configuración de URLs

--- horarios_backend/careers/urls/careers.py
+++ horarios_backend/careers/urls/careers.py
@@ -5,16 +5,10 @@ from careers.views import (
     CareerPaginatedView,
     CareerToggleStatusView,
 )
-from careers.views.groups import GroupCreate, GroupList, GroupDetail
 
 urlpatterns = [
     path('v1/university/careers/', CareerListView.as_view()),
     path('v1/university/careers/paginated/', CareerPaginatedView.as_view()),
     path('v1/university/careers/<int:pk>/', CareerDetailView.as_view()),
     path('v1/university/careers/<int:pk>/toggle-status/', CareerToggleStatusView.as_view()),
-
-    # Groups
-    path('v1/groups/', GroupList.as_view(), name='group-list'),
-    path('v1/groups/create/', GroupCreate.as_view(), name='group-create'),
-    path('v1/groups/<int:group_id>/', GroupDetail.as_view(), name='group-detail'),
 ]
--- horarios_backend/careers/urls/groups.py
+++ horarios_backend/careers/urls/groups.py
@@ -1,8 +1,27 @@
 from django.urls import path
-from careers.views.groups import GroupCreate, GroupList, GroupDetail
+
+from careers.views.groups import (
+    GroupDetailView,
+    GroupListView,
+    GroupPaginatedView,
+    GroupToggleStatusView,
+)
 
 urlpatterns = [
-    path('v1/groups/', GroupList.as_view(), name='group-list'),
-    path('v1/groups/create/', GroupCreate.as_view(), name='group-create'),
-    path('v1/groups/<int:group_id>/', GroupDetail.as_view(), name='group-detail'),
+    path('v1/university/groups/', GroupListView.as_view(), name='group-list'),
+    path(
+        'v1/university/groups/paginated/',
+        GroupPaginatedView.as_view(),
+        name='group-paginated',
+    ),
+    path(
+        'v1/university/groups/<int:pk>/toggle-status/',
+        GroupToggleStatusView.as_view(),
+        name='group-toggle-status',
+    ),
+    path(
+        'v1/university/groups/<int:pk>/',
+        GroupDetailView.as_view(),
+        name='group-detail',
+    ),
 ]
