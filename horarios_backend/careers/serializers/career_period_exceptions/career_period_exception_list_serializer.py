6bafcab2fd0b7215c99d29d1a0b023e2632ea00e
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-04-10 22:21:49 -0600

    [UPDATE] Se agregó el campo career_id a CareerPeriodExceptionListSerializer.

--- horarios_backend/careers/serializers/career_period_exceptions/career_period_exception_list_serializer.py
+++ horarios_backend/careers/serializers/career_period_exceptions/career_period_exception_list_serializer.py
@@ -5,4 +5,4 @@ from careers.models import CareerPeriodExceptions
 class CareerPeriodExceptionListSerializer(serializers.ModelSerializer):
     class Meta:
         model = CareerPeriodExceptions
-        fields = ['id', 'period_number', 'reason']
+        fields = ['id', 'career_id', 'period_number', 'reason']
